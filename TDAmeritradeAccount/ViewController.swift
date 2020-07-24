fff9ddab2897b88643c7ebc460b3a02c323b886a
Thach Do <td3pro@gmail.com>
2020-07-24 00:30:15 -0700

    Initial Commit

---
+++ TDAmeritradeAccount/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TDAmeritradeAccount
+//
+//  Created by Thach Do on 7/24/20.
+//  Copyright © 2020 Thach Do. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
